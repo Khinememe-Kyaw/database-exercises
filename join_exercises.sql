378349ad24bb8f31847d49a984ee5e2761aa615e
Khinememe Kyaw <khinememe.kyaw01@gmail.com>
2023-07-17 11:19:19 -0500

    Added ORDER BY after walkthrough

--- join_exercises.sql
+++ join_exercises.sql
@@ -13,7 +13,8 @@ SELECT d.dept_name AS "Department Name", CONCAT(e.first_name, ' ', e.last_name) 
 FROM departments AS d
          JOIN dept_manager AS dm ON d.dept_no = dm.dept_no
          JOIN employees AS e ON dm.emp_no = e.emp_no
-WHERE dm.to_date = '9999-01-01';
+WHERE dm.to_date = '9999-01-01'
+ORDER BY d.dept_name;
 
 SELECT d.dept_name AS "Department Name", CONCAT(e.first_name, ' ', e.last_name) AS "Department Manager"
 FROM departments AS d
